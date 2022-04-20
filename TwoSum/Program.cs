4d1db014280ae61764caf29c4a25de4cac99aa7a
Adam <adaost0110@hotmail.com>
2022-04-20 10:55:22 +0200

    Impementation and updates of notes

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TwoSum;
+using Xunit;
+
+var solution = new Solution();
+
+var inputArr = new int[] { 1, 1, 3, 2, 4 };
+var inputTarget = 6;
+var expected = new int[] { 3, 4 };
+
+var result = solution.TwoSum(inputArr, inputTarget);
+Assert.Equal(expected, result);
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TwoSum
+{
+    internal class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var arrLength = nums.Length;
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                var current = nums[i];
+                var difference = target - current;
+
+                if (dict.TryGetValue(difference, out var value))
+                {
+                    return new int[] { value, i };
+                }
+
+                dict[current] = i;
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
