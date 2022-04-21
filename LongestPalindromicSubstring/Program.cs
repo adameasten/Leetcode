05556dbb4bf881c2bb791796d6f30deb39f78d3f
Adam <adaost0110@hotmail.com>
2022-04-21 21:01:55 +0200

    Algoritm för att hitta längsta palindromiska substrängen

---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+
+var expected = "bb";
+var result = LongestPalindrome("cbbd");
+Assert.Equal(expected, result);
+
+static string LongestPalindrome(string s)
+{
+    var strlen = s.Length;
+
+    var result = string.Empty;
+    var resultLength = 0;
+
+    for (int i = 0; i < strlen; i++)
+    {
+        var rightIndex = i;
+        var leftIndex = i;
+
+        while (leftIndex >= 0 && rightIndex < strlen && (s[rightIndex] == s[leftIndex]))
+        {
+            var substringLength = rightIndex - leftIndex + 1;
+            if (substringLength > resultLength)
+            {
+                result = s.Substring(leftIndex, substringLength);
+                resultLength = substringLength;
+            }
+
+            rightIndex++;
+            leftIndex--;
+        }
+        
+        //Even numbers
+        rightIndex = i + 1;
+        leftIndex = i;
+
+        while (leftIndex >= 0 && rightIndex < strlen && (s[rightIndex] == s[leftIndex]))
+        {
+            var substringLength = rightIndex - leftIndex + 1;
+            if (substringLength > resultLength)
+            {
+                result = s.Substring(leftIndex, substringLength);
+                resultLength = substringLength;
+            }
+
+            rightIndex++;
+            leftIndex--;
+        }
+    }
+
+    return result;
+}
