c400c3bd32141255e7f48d3ca309f1a9b31960f9
Adam <adaost0110@hotmail.com>
2022-04-22 07:19:59 +0200

    Implementera algoritm för att hitta största arean mellan 2 bars inom en integer array

---
+++ ContainerWithMostWater/Program.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+var input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+var expected = 49;
+
+var result = MaxArea(input);
+Assert.Equal(expected, result);
+
+static int MaxArea(int[] height)
+{
+    int leftIndex = 0;
+    int rightIndex = height.Length - 1;
+    int maxArea = 0;
+
+    while (leftIndex < rightIndex)
+    {
+        var leftPillar = height[leftIndex];
+        var rightPillar = height[rightIndex];
+
+        var lowest = leftPillar > rightPillar ? rightPillar : leftPillar;
+        var area = lowest * (rightIndex - leftIndex);
+
+        if (area > maxArea)
+            maxArea = area;
+
+        if (lowest == leftPillar)
+            leftIndex++;
+        else
+            rightIndex--;
+    }
+
+    return maxArea;
+}
