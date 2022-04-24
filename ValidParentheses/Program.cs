52e4887602a22728dce4ee93ff78aed26efc5af4
Adam <adaost0110@hotmail.com>
2022-04-24 23:26:16 +0200

    Lagt in en algorithm som kollar om input strängen enbart innehåller valid paranteser

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+var result = IsValid("()[]{}");
+Assert.Equal(true, result);
+
+static bool IsValid(string s)
+{
+    if (s.Length % 2 != 0)
+        return false;
+
+    var stack = new Stack<char>();
+
+    foreach (var item in s)
+    {
+        if (item == '(')
+            stack.Push(')');
+        else if (item == '[')
+            stack.Push(']');
+        else if (item == '{')
+            stack.Push('}');
+        else if (stack.Count == 0 || stack.Pop() != item)
+            return false;
+    }
+
+    return stack.Count == 0;
+}
