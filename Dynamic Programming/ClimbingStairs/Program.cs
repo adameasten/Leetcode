29a3ac572aa35a29c040b565ed52312b7876e58d
Adam <adaost0110@hotmail.com>
2022-05-04 07:40:14 +0200

    Added dyniamc programming algoritm and notes

---
+++ Dynamic Programming/ClimbingStairs/Program.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+var expected = 3;
+var result = ClimbStairs(3);
+
+Assert.Equal(expected, result);
+
+static int ClimbStairs(int n)
+{
+    var one = 1;
+    var two = 1;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        var temp = one;
+        one += two;
+        two = temp;
+    }
+
+    return one;
+}
