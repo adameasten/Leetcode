2b7a450bedc52d8773544655139d8dddadb7b15c
Adam <adaost0110@hotmail.com>
2022-05-06 06:59:34 +0200

    omstrukturering samt algoritm för att hitta produkten av alla input values except de index man står på

---
+++ Array/ProdOfArrExceptSelf/Program.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+var input = new int[] { 1, 2, 3, 4 };
+var result = ProductExceptSelf(input);
+var expected = new int[] { 24, 12, 8, 6 };
+
+Assert.Equal(expected, result);
+
+static int[] ProductExceptSelf(int[] nums)
+{
+    var result = new int[nums.Length];
+    var preNumSum = 1;
+    var postNumSum = 1;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = preNumSum;
+        preNumSum *= nums[i];
+    }
+
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        result[i] *= postNumSum;
+        postNumSum *= nums[i];
+    }
+
+    return result;
+}
