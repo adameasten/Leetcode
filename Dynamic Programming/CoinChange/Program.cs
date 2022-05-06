8baf49b4f9a2017670299f7996538433cd8cbe55
Adam <adaost0110@hotmail.com>
2022-05-06 06:12:32 +0200

    Lagt in DP aloritm

---
+++ Dynamic Programming/CoinChange/Program.cs
@@ -0,0 +1,27 @@
+﻿var coins = new int[] { 2 };
+var amount = 3;
+var result = CoinChange(coins, amount);
+
+static int CoinChange(int[] coins, int finalAmount)
+{
+    var dp = new int[finalAmount + 1];
+
+    for (var currentAmount = 1; currentAmount <= finalAmount; currentAmount++)
+    {
+        dp[currentAmount] = int.MaxValue;
+        foreach (var coin in coins)
+        {
+            var diff = currentAmount - coin;
+
+            if (diff >= 0 && dp[diff] != int.MaxValue)
+            {
+                dp[currentAmount] = Math.Min(dp[currentAmount], 1 + dp[diff]);
+            }
+        }
+    }
+
+    if (dp[finalAmount] != int.MaxValue)
+        return dp[finalAmount];
+    else
+        return -1;
+}
